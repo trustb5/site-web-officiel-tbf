a63ce2d50296df7228cd8e555fcf820d3afe5279
technical support <technicalsupport@trustbusinessfive.org>
2025-02-05 18:23:09 +0100

    Static root

--- tbf/settings.py
+++ tbf/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'stati']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
